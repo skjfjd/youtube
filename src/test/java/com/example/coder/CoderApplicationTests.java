7f40d42fe6dddb77c14a5f7cd6cf65e57d9f187e
송진하 <songjinha@songjinhaui-MacBookPro.local>
2021-04-18 22:51:52 +0900

    first commit

---
+++ src/test/java/com/example/coder/CoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
